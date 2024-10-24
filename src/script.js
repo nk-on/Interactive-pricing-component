9133b26b3e4bc8205ba97f5adc11467409f84588
nk-on <johndoe@example.com>
2024-10-24 21:59:15 +0400

    Feat:when switching toogle button colorizing billing option containers

--- src/script.js
+++ src/script.js
@@ -15,9 +15,13 @@ priceRange.addEventListener('input',()=>{
 });
 switchButton.addEventListener('click',()=>{
   if(switchButton.classList.contains('toogle-unswitched')){
+    monthlyBillingContainer.style.color = '#10D8C4';
+    yearlyBillingContainer.style.color = '#848EAD';
     switchButton.classList.remove('toogle-unswitched');
     switchButton.classList.add('toogle-switched');
   }else{
+    yearlyBillingContainer.style.color = '#10D8C4';
+    monthlyBillingContainer.style.color = '#848EAD';
     switchButton.classList.remove('toogle-switched');
     switchButton.classList.add('toogle-unswitched');
   }
